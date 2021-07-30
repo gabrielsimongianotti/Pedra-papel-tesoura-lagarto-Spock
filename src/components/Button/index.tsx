332a0d8b3cc5d92c7e167837fd0fac26f6112149
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-07-30 17:01:07 -0300

    feat(button): create component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
 
-export default function Dashboard() {
-  return (
-    <>
-   
-    </>
-  );
+import { Container, ButtonText } from './styles';
+
+interface ButtonsProps extends RectButtonProperties {
+  title: string;
 }
+
+const Button: React.FC<ButtonsProps> = ({ title, ...rest }) => (
+  <Container {...rest}>
+    <ButtonText>
+      {title}
+    </ButtonText> 
+  </Container>
+);
+
+export default Button;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: calc(100%- 20px);
+  height: 60px;
+  background: #373E4E;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin:20px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #a5a7ac;
+  font-size: 18px
+`;
