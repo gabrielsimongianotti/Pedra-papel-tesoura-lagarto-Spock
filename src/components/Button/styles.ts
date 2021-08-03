3aed042de3a5494df3ce554fb5663a3e80bf5a91
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-08-03 17:43:55 -0300

    fix:remove calc and correction syntax error

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,16 +2,16 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: calc(100%- 20px);
+  width: 100%;
   height: 60px;
   background: #373E4E;
   border-radius: 10px;
   justify-content: center;
   align-items: center;
-  margin:20px;
+  margin: 20px;
 `;
 
 export const ButtonText = styled.Text`
   color: #a5a7ac;
-  font-size: 18px
+  font-size: 18px;
 `;
